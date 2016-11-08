ec79c1b14f6fc68d8043ed359fcb1505a8068de9
lenis0012 <0p1q9o2w@hotmail.nl>
2016-11-08 22:12:49 +0100

    add setters

--- src/main/java/com/lenis0012/chatango/api/config/builder/ChatangoConfigBuilder.java
+++ src/main/java/com/lenis0012/chatango/api/config/builder/ChatangoConfigBuilder.java
@@ -6,4 +6,19 @@ public abstract class ChatangoConfigBuilder extends AbstractConfigBuilder<Chatan
     protected boolean threadPerRoom = true;
     protected int threadPoolCount = 1;
     protected boolean autoReconnect = true;
+
+    public ChatangoConfigBuilder threadPerRoom(boolean threadPerRoom) {
+        this.threadPerRoom = threadPerRoom;
+        return this;
+    }
+
+    public ChatangoConfigBuilder threadPoolCount(int threadPoolCount) {
+        this.threadPoolCount = threadPoolCount;
+        return this;
+    }
+
+    public ChatangoConfigBuilder autoReconnect(boolean autoReconnect) {
+        this.autoReconnect = autoReconnect;
+        return this;
+    }
 }
